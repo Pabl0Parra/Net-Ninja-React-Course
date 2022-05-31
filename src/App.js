472435db9f6ae554392692d8f5719ee1092dc73f
Pabl0Parra <98957023+Pabl0Parra@users.noreply.github.com>
2022-05-31 09:57:11 +0200

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; //we always export the function so that it can be reused elsewhere.
+
+
+// { title } = dynamic variable or value. It returns whatever is inside as a string!
+// we can write a string, number or array inside the curly brackets but not objects or boolean variables
